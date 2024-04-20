173f0befd0727092fbf6908b99c845a5aa71fcfb
DevAtShafiq <98973818+DevAtShafiq@users.noreply.github.com>
2024-04-20 17:11:22 +0900

    regex added

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
 
+//
+//        TravelApp app = new TravelApp();
+//
+//        app.bookFlight(1010000,"shafiq","dhaka","incheon","2024-04-08",2,52000.00);
+//        app.bookHotel(101010,"shafiq","dhaka","incheon","2024-04-08",2,52000.00);
+//        app.cancelReservation(101010);
+//
+//
+//        for (Hotel hotel:app.hotels){
+//
+//            System.out.println(hotel.getHotelId()+hotel.getName());
+//        }
+//        for (Flight flight:app.flights){
+//
+//            System.out.println(flight.getFlightNumber());
+//        }
 
-        TravelApp app = new TravelApp();
-
-        app.bookFlight(1010000,"shafiq","dhaka","incheon","2024-04-08",2,52000.00);
-        app.bookHotel(101010,"shafiq","dhaka","incheon","2024-04-08",2,52000.00);
-        app.cancelReservation(101010);
 
 
-        for (Hotel hotel:app.hotels){
+        String pattern="[A-Z]";
 
-            System.out.println(hotel.getHotelId()+hotel.getName());
+        Pattern p =Pattern.compile(pattern);
+
+        Matcher matcher=p.matcher("Hello world");
+        List<String>letters=new ArrayList<>();
+        while (matcher.find()){
+            letters.add(matcher.group());
         }
-        for (Flight flight:app.flights){
+        System.out.println(letters);
 
-            System.out.println(flight.getFlightNumber());
-        }
+
+
+
     }
 }
